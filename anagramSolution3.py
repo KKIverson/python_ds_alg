b9b54da06fbf69c369881773882d611cf3bee9e2
Liukai <liukaisysu@foxmail.com>
2020-03-16 20:15:31 +0800

    add anagramsSolution3.py

---
+++ anagramSolution3.py
@@ -0,0 +1,23 @@
+# using Counter: O(n) solution
+# space in exchange of time
+# from collections import Counter
+def anagramSolution3(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    # c1 = Counter(s1)
+    # c2 = Counter(s2)
+    c1 = [0] * 26
+    c2 = [0] * 26
+    for i in range(len(s1)):
+        c1[ord(s1[i]) - ord('a')] += 1
+        c2[ord(s2[i]) - ord('a')] += 1
+    stillOK = True
+    j = 0
+    while j < 26 and stillOK:
+        if c1[j] != c2[j]:
+            stillOK = False
+            break
+        j += 1
+    # return c1 == c2
+    return stillOK
+print(anagramSolution3('apple', 'plfap'))
